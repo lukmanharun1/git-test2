d887c5d06a5da3bd6f5fa63028511250000c05bf
lukman <nandes88.ni@gmail.com>
2020-07-31 16:08:26 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
   <h1>Hello World!</h1>
   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis totam quaerat cum nihil facilis voluptatem nemo similique voluptatum nesciunt, nam quidem beatae quae vero nulla cumque adipisci est iure dolores.</p>
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+  </ul>
 </body>
 
 </html>
