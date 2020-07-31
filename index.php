9c506939468fa256ea51188c0d232dad4abf09bf
lukman <nandes88.ni@gmail.com>
2020-07-31 16:03:24 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
 <body>
   <h1>Hello World!</h1>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis totam quaerat cum nihil facilis voluptatem nemo similique voluptatum nesciunt, nam quidem beatae quae vero nulla cumque adipisci est iure dolores.</p>
 </body>
 
 </html>
